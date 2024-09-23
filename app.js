be2d260e28f2ad5ee9d6be1cac5b7591fe925b87
jvnagaoo <127306277+jvnagaoo@users.noreply.github.com>
2024-09-23 12:22:47 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("Boas vindas ao jogo do numero secreto");
+let numeroSecreto = 5;
+let chute = prompt("escolha um numero entre 1 e 10");
+if (chute == numeroSecreto)
+{
+    console.log("isso aí!!Você descobriu o numero secreto")
+}
